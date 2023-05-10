d44e2e8bd51bde87ae091bf90478214ee26c9c6f
HarryHaha1010 <tung.huynhthanhkhmt@hcmut.edu.vn>
2023-05-10 15:59:09 +0700

    connect Ada and app

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,8 +1,55 @@
-import React, {useState} from 'react'
+import React, {useEffect,useState} from 'react'
 import { View, Text, TouchableOpacity, Switch } from 'react-native'
 import { Button, Header } from "react-native-elements"
 import styles from '../../elements/style'
+import debounce from 'lodash/debounce';
 const HomePage: React.FC =() =>{
+    const [temperature, setTemperature] = useState(null);
+    const [light, setLight] = useState(null);
+    const [humidity, setHumidity] = useState(null);
+    const debouncedFetch = debounce(() => {
+        // Fetch temperature data
+        fetch(`https://io.adafruit.com/api/v2/HarryHaha/feeds/temp-sensor/data/last`,
+          {
+            headers: {
+              'X-AIO-Key': 'aio_OepY02kt3vhaTPfCPadrdw7azvV7'
+            }
+          })
+          .then(response => response.json())
+          .then(data => setTemperature(data.value))
+          .catch(error => console.error(error));
+      
+        // Fetch light data
+        fetch(`https://io.adafruit.com/api/v2/HarryHaha/feeds/light-sensor/data/last`,
+          {
+            headers: {
+              'X-AIO-Key': 'aio_OepY02kt3vhaTPfCPadrdw7azvV7'
+            }
+          })
+          .then(response => response.json())
+          .then(data => setLight(data.value))
+          .catch(error => console.error(error));
+      
+        // Fetch humidity data
+        fetch(`https://io.adafruit.com/api/v2/HarryHaha/feeds/humid-sensor/data/last`,
+          {
+            headers: {
+              'X-AIO-Key': 'aio_OepY02kt3vhaTPfCPadrdw7azvV7'
+            }
+          })
+          .then(response => response.json())
+          .then(data => setHumidity(data.value))
+          .catch(error => console.error(error));
+      }, 100);
+    
+
+    useEffect(() => {
+        debouncedFetch();
+      
+        const intervalId = setInterval(debouncedFetch, 5000);
+      
+        return () => clearInterval(intervalId);
+      }, []);
     return(
         <View>
             <Header
@@ -24,15 +71,15 @@ const HomePage: React.FC =() =>{
             <View style = {styles.container}>
                 <Text style = {styles.title}>Temperature</Text>
                 <TouchableOpacity style={styles.buttonStyle}>
-                    <Text style = {styles.text_mid}>25</Text>
+                    <Text style={styles.text_mid}>{temperature != null ? temperature : "-"}</Text>
                 </TouchableOpacity>
                 <Text style = {styles.title}>Brightness</Text>
                 <TouchableOpacity style={styles.buttonStyle}>
-                    <Text style = {styles.text_mid}>50</Text>
+                    <Text style = {styles.text_mid}>{light != null ? light : "-"}</Text>
                 </TouchableOpacity>
                 <Text style = {styles.title}>Humidity</Text>
                 <TouchableOpacity style={styles.buttonStyle}>
-                    <Text style = {styles.text_mid}>80%</Text>
+                    <Text style = {styles.text_mid}>{humidity != null ? humidity : "-"}%</Text>
                 </TouchableOpacity>
             </View>
 
